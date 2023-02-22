2981d8def3e55770ffa50bb6d7595e3a5042e531
tmarkec <tmarkec@gmail.com>
2023-02-22 11:25:52 +0000

    debug true

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,11 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -34,7 +33,7 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
-
+    
     def post(self, request, slug, *args, **kwargs):
 
         queryset = Post.objects.filter(status=1)
@@ -50,7 +49,6 @@ class PostDetail(View):
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.post = post
-            messages.success(request, 'Comment succesfully posted')
             comment.save()
         else:
             comment_form = CommentForm()
@@ -69,12 +67,12 @@ class PostDetail(View):
 
 
 class PostLike(View):
+    
     def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
-
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-        
+
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
